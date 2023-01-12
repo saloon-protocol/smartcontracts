e0fee4741fe8a824ba635d448b734fd41098f933
fatherGoose1 <0xdjangoonchain@protonmail.com>
2023-01-11 19:22:16 -0500

    Raw stargate strategy complete

--- src/StargateStrategy.sol
+++ src/StargateStrategy.sol
@@ -9,24 +9,30 @@ import "./lib/OwnableUpgradeable.sol";
 import "./IStargateRouter.sol";
 import "./IStargateLPStaking.sol";
 import "./IStargateLPToken.sol";
+import "./IUniswapRouter.sol";
 
 /* Implement:
 - TODO add back ownable
+- TODO Slippage control
 */
 
 contract StargateStrategy is OwnableUpgradeable {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
-    uint256 lpDepositBalance;
+    uint256 public lpDepositBalance;
 
     IStargateRouter public stargateRouter;
     IStargateLPStaking public stargateLPStaking;
     IStargateLPToken public stargateLPToken;
+    IUniswapRouter public uniswapRouter;
     IERC20 public constant USDC =
         IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
     IERC20 public constant STG =
         IERC20(0xAf5191B0De278C7286d6C7CC6ab6BB8A73bA2Cd6);
+    address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    uint24 public constant poolFee = 3000;
 
     constructor() {
         stargateRouter = IStargateRouter(
@@ -38,34 +44,121 @@ contract StargateStrategy is OwnableUpgradeable {
         stargateLPToken = IStargateLPToken(
             0xdf0770dF86a8034b3EFEf0A1Bb3c889B8332FF56
         ); //S*USDC
+        uniswapRouter = IUniswapRouter(
+            0xE592427A0AEce92De3Edee1F18E0157C05861564
+        );
         // __Ownable_init();
     }
 
     function despositToStrategy(uint256 _poolId, uint256 _amount)
-        external
+        public
         returns (
-            // onlyOwner
+            // onlyOwnerOrThis
             uint256
         )
     {
-        USDC.safeTransferFrom(msg.sender, address(this), _amount);
+        if (msg.sender != address(this)) {
+            USDC.safeTransferFrom(msg.sender, address(this), _amount);
+        }
         USDC.approve(address(stargateRouter), _amount);
 
         stargateRouter.addLiquidity(_poolId, _amount, address(this));
-        uint256 lpBalance = stargateLPToken.balanceOf(address(this));
+        uint256 lpBalanceAdded = stargateLPToken.balanceOf(address(this));
 
-        stargateLPToken.approve(address(stargateLPStaking), lpBalance);
-        stargateLPStaking.deposit(0, lpBalance); // PID 0 is S*USDC
-        lpDepositBalance += lpBalance;
+        stargateLPToken.approve(address(stargateLPStaking), lpBalanceAdded);
+        stargateLPStaking.deposit(0, lpBalanceAdded); // PID 0 is S*USDC
+        lpDepositBalance += lpBalanceAdded;
 
-        return lpDepositBalance;
+        return lpBalanceAdded;
     }
 
-    function rewardBalance() external view returns (uint256) {
+    // Needs onlyOwner
+    function withdrawFromStrategy(uint256 _poolId, uint256 _amount)
+        external
+        returns (uint256)
+    {
+        require(_amount <= lpDepositBalance, "Not enough lp");
+        lpDepositBalance -= _amount;
+        stargateLPStaking.withdraw(0, _amount);
+        uint256 lpBalanceAvailable = stargateLPToken.balanceOf(address(this));
+        uint256 amountReturned = stargateRouter.instantRedeemLocal(
+            uint16(_poolId),
+            lpBalanceAvailable,
+            address(this)
+        );
+        convertReward();
+        uint256 USDCBalance = USDC.balanceOf(address(this));
+        USDC.safeTransfer(msg.sender, USDCBalance);
+
+        return USDCBalance;
+    }
+
+    // Needs onlyOwner
+    function compound() external returns (uint256) {
+        stargateLPStaking.deposit(0, 0);
+        uint256 returnedAmount = convertReward();
+
+        uint256 lpAdded = this.despositToStrategy(1, returnedAmount);
+
+        return lpAdded;
+    }
+
+    function convertReward() public returns (uint256) {
+        uint256 availableSTG = rewardBalance();
+        if (availableSTG == 0) return 0;
+        STG.approve(address(uniswapRouter), availableSTG);
+
+        IUniswapRouter.ExactInputParams memory params = IUniswapRouter
+            .ExactInputParams({
+                path: abi.encodePacked(
+                    address(STG),
+                    poolFee,
+                    WETH9,
+                    poolFee,
+                    address(USDC)
+                ),
+                recipient: address(this),
+                deadline: block.timestamp,
+                amountIn: availableSTG,
+                amountOutMinimum: 0
+            });
+        uint256 returnedAmount = uniswapRouter.exactInput(params);
+
+        return returnedAmount;
+    }
+
+    function rewardBalance() public view returns (uint256) {
         return STG.balanceOf(address(this));
     }
 
     function pendingRewardBalance() external view returns (uint256) {
         return stargateLPStaking.pendingStargate(0, address(this)); // PID 0 is S*USDC
+    }
+
+    function userInfo() external view returns (uint256, uint256) {
+        (uint256 amount, uint256 rewardDebt) = stargateLPStaking.userInfo(
+            0,
+            address(this)
+        );
+        return (amount, rewardDebt);
+    }
+
+    function poolInfo()
+        external
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256,
+            uint256
+        )
+    {
+        (
+            uint256 lpToken,
+            uint256 allocPoint,
+            uint256 lastRewardBlock,
+            uint256 accStargatePerShare
+        ) = stargateLPStaking.poolInfo(0);
+        return (lpToken, allocPoint, lastRewardBlock, accStargatePerShare);
     }
 }
--- src/test/StargateStrategy.t.sol
+++ src/test/StargateStrategy.t.sol
@@ -11,7 +11,6 @@ contract StargateStrategyTest is DSTest, Script {
 
     ERC20 USDC = ERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
     address USDCHolder = address(0x451AbAc74B2Ef32790C20817785Dd634f9217D5a);
-    address deployer;
 
     function setUp() external {
         string memory rpc = vm.envString("ETH_RPC_URL");
@@ -20,21 +19,19 @@ contract StargateStrategyTest is DSTest, Script {
 
         stargateStrategy = new StargateStrategy();
 
-        deployer = address(this);
-
         vm.deal(USDCHolder, 500 ether);
-        vm.deal(deployer, 500 ether);
+        vm.deal(address(this), 500 ether);
 
-        // USDC holder transfers deployer 100 USDC for testing
-        vm.startPrank(USDCHolder);
-        USDC.transfer(deployer, 100 * (10**6));
+        // USDC holder transfers address(this) 100 USDC for testing
+        vm.prank(USDCHolder);
+        USDC.transfer(address(this), 100 * (10**6));
     }
 
     // ============================
     // Test Implementation Update
     // ============================
     function testDeposit() external {
-        uint256 USDCBalance = USDC.balanceOf(deployer);
+        uint256 USDCBalance = USDC.balanceOf(address(this));
         USDC.approve(address(stargateStrategy), USDCBalance);
         uint256 lpDepositBalance = stargateStrategy.despositToStrategy(
             1,
@@ -44,9 +41,65 @@ contract StargateStrategyTest is DSTest, Script {
         // LP tokens aren't minted 1:1, so only checking that the returned LP is close to input.
         assert(balanceDiff < int256(USDCBalance) / 100);
 
-        vm.warp(block.timestamp + 7 days);
-        stargateStrategy.despositToStrategy(1, 0);
-        uint256 STGBalance = stargateStrategy.pendingRewardBalance();
-        assertEq(STGBalance, 5);
+        (uint256 amount, uint256 rewardDebt) = stargateStrategy.userInfo();
+        (
+            uint256 lpToken,
+            uint256 allocPoint,
+            uint256 lastRewardBlock,
+            uint256 accStargatePerShare
+        ) = stargateStrategy.poolInfo();
+        assertEq(lastRewardBlock, block.number);
+
+        vm.roll(block.number + 1000); // STG rewards based on passed blocks, not timestamp
+        uint256 pendingRewards = stargateStrategy.pendingRewardBalance();
+        assert(pendingRewards > 0);
+    }
+
+    function testWithdraw() external {
+        uint256 USDCBalance = USDC.balanceOf(address(this));
+        USDC.approve(address(stargateStrategy), USDCBalance);
+        uint256 lpAdded = stargateStrategy.despositToStrategy(1, USDCBalance);
+
+        vm.roll(block.number + 1000); // STG rewards based on passed blocks, not timestamp
+        uint256 amountWithdrawn = stargateStrategy.withdrawFromStrategy(
+            1,
+            lpAdded
+        );
+
+        uint256 finalUSDCBalance = USDC.balanceOf(address(this));
+        assert(finalUSDCBalance >= USDCBalance); // Small 1 wei (USDC) precision loss
+    }
+
+    function testCompound() external {
+        uint256 USDCBalance = USDC.balanceOf(address(this));
+        USDC.approve(address(stargateStrategy), USDCBalance);
+        stargateStrategy.despositToStrategy(1, USDCBalance);
+
+        uint256 lpDepositBalanceInitial = stargateStrategy.lpDepositBalance();
+
+        vm.roll(block.number + 100); // STG rewards based on passed blocks, not timestamp
+        uint256 lpAdded = stargateStrategy.compound();
+        assert(lpAdded > 0); // Small 1 wei (USDC) precision loss
+
+        uint256 lpDepositBalanceFinal = stargateStrategy.lpDepositBalance();
+        assert(lpDepositBalanceFinal > lpDepositBalanceInitial);
+        assertEq(lpDepositBalanceFinal, lpDepositBalanceInitial + lpAdded);
+    }
+
+    function testDepositCompoundWithdraw() external {
+        uint256 USDCBalanceInitial = USDC.balanceOf(address(this));
+        USDC.approve(address(stargateStrategy), USDCBalanceInitial);
+        stargateStrategy.despositToStrategy(1, USDCBalanceInitial);
+
+        uint256 lpDepositBalanceInitial = stargateStrategy.lpDepositBalance();
+
+        vm.roll(block.number + 1000); // STG rewards based on passed blocks, not timestamp
+        stargateStrategy.compound();
+
+        uint256 lpDepositBalanceIntermediate = stargateStrategy
+            .lpDepositBalance();
+        stargateStrategy.withdrawFromStrategy(1, lpDepositBalanceIntermediate);
+        uint256 USDCBalanceFinal = USDC.balanceOf(address(this));
+        assert(USDCBalanceFinal > USDCBalanceInitial);
     }
 }
