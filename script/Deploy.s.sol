25703e1764191a5fe44c5979bbadf6f8c20713be
fatherGoose1 <0xdjangoonchain@protonmail.com>
2023-02-20 19:34:57 -0500

    Committing script changes

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,54 +1,56 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.10;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
 
-// import "forge-std/Script.sol";
-// import "../src/SaloonProxy.sol";
-// import "../src/Saloon.sol";
-// import "../src/StrategyFactory.sol";
-// import "openzeppelin-contracts/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
+import "forge-std/Script.sol";
+import "../src/SaloonProxy.sol";
+import "../src/Saloon.sol";
+import "../src/BountyTokenNFT.sol";
+import "../src/StrategyFactory.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
 
-// contract Deploy is Script {
-//     function run() external {
-//         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-//         vm.startBroadcast(deployerPrivateKey);
+contract Deploy is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
 
-//         // ERC20PresetFixedSupply USDC = ERC20PresetFixedSupply(
-//         //     0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d
-//         // ); // Mainnet
-//         // address projectWallet = 0x84bB382457299Ed13E946529E010ee54Cfa047ab; // Mainnet
-//         address projectWallet = 0x1B7FE02Da6c7a7175a33D109397492c2872c6A5e; // Testnet
-//         bytes memory data = "";
+        // ERC20PresetFixedSupply USDC = ERC20PresetFixedSupply(
+        //     0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d
+        // ); // Mainnet
+        // address projectWallet = 0x84bB382457299Ed13E946529E010ee54Cfa047ab; // Mainnet
+        address projectWallet = 0x1B7FE02Da6c7a7175a33D109397492c2872c6A5e; // Testnet
+        bytes memory data = "";
 
-//         Saloon saloonImplementation = new Saloon();
-//         SaloonProxy saloonProxy = new SaloonProxy(
-//             address(saloonImplementation),
-//             data
-//         );
-//         Saloon saloon = Saloon(address(saloonProxy));
-//         StrategyFactory factory = new StrategyFactory();
-//         saloon.initialize(address(factory));
+        Saloon saloonImplementation = new Saloon();
+        SaloonProxy saloonProxy = new SaloonProxy(
+            address(saloonImplementation),
+            data
+        );
+        Saloon saloon = Saloon(address(saloonProxy));
+        StrategyFactory factory = new StrategyFactory();
+        BountyTokenNFT bountyToken = new BountyTokenNFT(address(saloonProxy));
+        saloon.initialize(address(bountyToken), address(factory));
 
-//         // // START TESTNET
-//         ERC20PresetFixedSupply USDC = new ERC20PresetFixedSupply(
-//             "Saloon USDC",
-//             "SUSDC",
-//             10000000 ether,
-//             projectWallet
-//         );
-//         // // END TESTNET
+        // // START TESTNET
+        ERC20PresetFixedSupply USDC = new ERC20PresetFixedSupply(
+            "Saloon USDC",
+            "SUSDC",
+            10000000 ether,
+            projectWallet
+        );
+        // // END TESTNET
 
-//         saloon.updateTokenWhitelist(address(USDC), true, 10 * 10**6);
-//         USDC.approve(address(saloonProxy), 1000 ether);
-//         uint256 pid = saloon.addNewBountyPool(
-//             address(USDC),
-//             projectWallet,
-//             "Saloon",
-//             address(0),
-//             0,
-//             0
-//         );
-//         saloon.setAPYandPoolCapAndDeposit(pid, 10000 ether, 4700, 0 ether, "");
+        saloon.updateTokenWhitelist(address(USDC), true, 10 * 10**6);
+        USDC.approve(address(saloonProxy), 1000 ether);
+        uint256 pid = saloon.addNewBountyPool(
+            address(USDC),
+            projectWallet,
+            "Saloon",
+            address(0),
+            0,
+            0
+        );
+        saloon.setAPYandPoolCapAndDeposit(pid, 10000 ether, 4700, 0 ether, "");
 
-//         vm.stopBroadcast();
-//     }
-// }
+        vm.stopBroadcast();
+    }
+}
